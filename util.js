ce4cc4c7b5c34cc01a8eaa26e2ec899f18ddfffa
diocai <caijiabin2008@gmail.com>
2016-10-17 17:09:11 +0800

    新增对数组去重的函数uniqArray()

--- util.js
+++ util.js
@@ -34,7 +34,7 @@ function cloneObject(src) {
         case '[object Array]':
             obj = new Array();
             for (var i = 0; i < src.length; i++) {
-                obj[i] = src[i];
+                obj.push(src[i]);
             }
             break;
         default:
@@ -46,64 +46,91 @@ function cloneObject(src) {
         return obj;
     }
 }
-    /* 
-    //使用if语句写的函数
-        if ((objType === '[object Number]')||(objType === '[object String]')||(objType === '[object Boolean]')||(objType === '[object Date]')){
-            var obj = src;
-            return obj;
-        }
-    
-        if (objType === '[object Object]'){
-            var obj = new Object();
-            for (var propName in src){
-                //用递归的方式判断src[proName]是什么类型的数据，然后再返回给新建的obj[propName]
-                //令propName="Name"，则Object[propName]就相当于Object.Name；这是一个用变量访问对象属性的方法
-                //错误的函数之一、我用obj.propName = src.propName做赋值，这样相当于把src的propName属性赋值给obj的probName属性
-                //错误的函数之二、我在for-in中写了obj[propName]=src[propName]，这样两者的propName属性公用一个内存地址，修改其中一个，另一个就会跟着变动
-                obj[propName] = cloneObject(src[propName]);          
-            }
-            return obj;
-        }
-    
-    
-        if (objType === '[object Array]'){
-            var obj = new Array();
-            for (var i=0;i<src.length;i++){
-                obj[i]=src[i];
-            }
-            return obj;
-        }
-    */
-
-
-
-
-
-// 测试用例：
-var srcObj = {
-    a: 1,
-    b: {
-        b1: ["hello", "hi"],
-        b2: "JavaScript",
-        b3: true,
-        b4: new Date(Date.now()),
-        b5: /gi/i,
-        b6: isArray
+/* 
+//使用if语句写的函数
+    if ((objType === '[object Number]')||(objType === '[object String]')||(objType === '[object Boolean]')||(objType === '[object Date]')){
+        var obj = src;
+        return obj;
     }
-};
-var abObj = srcObj;
-var tarObj = cloneObject(srcObj);
+ 
+    if (objType === '[object Object]'){
+        var obj = new Object();
+        for (var propName in src){
+            //用递归的方式判断src[proName]是什么类型的数据，然后再返回给新建的obj[propName]
+            //令propName="Name"，则Object[propName]就相当于Object.Name；这是一个用变量访问对象属性的方法
+            //错误的函数之一、我用obj.propName = src.propName做赋值，这样相当于把src的propName属性赋值给obj的probName属性
+            //错误的函数之二、我在for-in中写了obj[propName]=src[propName]，这样两者的propName属性公用一个内存地址，修改其中一个，另一个就会跟着变动
+            obj[propName] = cloneObject(src[propName]);          
+        }
+        return obj;
+    }
+ 
+ 
+    if (objType === '[object Array]'){
+        var obj = new Array();
+        for (var i=0;i<src.length;i++){
+            obj[i]=src[i];
+        }
+        return obj;
+    }
+*/
 
-srcObj.a = 2;
-srcObj.b.b1[0] = "Hello";
-srcObj.b.b3 = false;
 
-console.log(abObj.a);
-console.log(abObj.b.b1[0]);
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(arr) {
+    var arrNew = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrNew.push(arr[i]);
+    }
+    for (var i = 0; i < arrNew.length; i++) {
+        for (var j = 1; j < arrNew.length; j++) {
+            // i!=j 防止把最后一个元素也删除了
+            // j-=1 由于arrNew.splice(j,1)删除了一个元素[j]，那么原本的[j]以后的元素就会往前进一个元素
+            if (i != j) {
+                if (arrNew[i] === arrNew[j]) {
+                    arrNew.splice(j, 1); // arr.splice(j, n)，删除arr[j]及以后的n-1个元素，arr变成删除后的数列，slice对arr不造成影响
+                    j -= 1;
+                }
+            }
+        }
+    }
+    return arrNew;
+}
 
-console.log(tarObj.a);      // 1
-console.log(tarObj.b.b1[0]);    // "hello"
+// 中级班同学跳过此题
+// 实现一个简单的trim函数，用于去除一个字符串，头部和尾部的空白字符
+// 假定空白字符只有半角空格、Tab
+// 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
+function simpleTrim(str) {
+    return str;
+}
+// 使用示例
 
-console.log(abObj);
-console.log(tarObj);
+var text = "mom and dad and baby";
+var pattern = /mom( and dad( and baby)?)?/gi;
+var matches = pattern.exec(text);
+console.log(matches.index); // 0
+console.log(matches.input); // "mom and dad and baby"
+console.log(matches[0]); // "mom and dad and baby"
+console.log(matches[1]); // " and dad and baby"
+console.log(matches[2]); // " and baby"
 
+console.log(matches);
+
+
+var text = "cat, bat, sat, fat";
+var pattern1 = /.at/g;
+var arr = [];
+
+while (pattern1.lastIndex < text.length) {
+    var a = pattern1.exec(text);
+    arr=arr.concat(a);
+    console.log(a);
+}
+console.log(arr);
+
+var text = "000-00-0000";
+var pattern = /\d{3}-\d{2}-\d{4}/;
+if (pattern.test(text)){
+console.log("The pattern was matched.");
+}
