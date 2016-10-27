6f07378872c9ce963a0cd4f8f6241ce0a662a740
d4despair <caijiabin2008@gmail.com>
2016-10-27 23:33:45 +0800

    二叉树遍历初步成型

--- task22/task22.js
+++ task22/task22.js
@@ -3,39 +3,73 @@ function $(id) {
     return document.getElementById(id);
 }
 
-function Node(data, left, right) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
+function Tree(element) {
+    this._root = element;
+}
 
-    if (this.left) {
-        var data = this.left;
-        var left = data.children[0];
-        var right = data.children[1];
-        this.left = new Node(data, left, right);
-    };
-    if (this.right) {
-        var data = this.right;
-        var left = data.children[0];
-        var right = data.children[1];
-        this.right = new Node(data, left, right);
-    };
-
-
-    this.show = function () {
-        return this.data;//显示保存在节点中的数据
+Tree.prototype = {
+    constructor: Tree,
+    traveseDLR: function (callback) {
+        (function recurse(current) {
+                callback.call(this, current);
+            if (current.children.length >= 1) {
+                recurse(current.children[0]);
+            }
+            if (current.children.length >= 2) {
+                recurse(current.children[1]);
+            }
+        })(this._root);
+    },
+    traveseLDR: function (callback) {
+        (function recurse(current) {
+            if (current.children.length >= 1) {
+                recurse(current.children[0]);
+            }
+            callback.call(this, current);
+            if (current.children.length >= 2) {
+                recurse(current.children[1]);
+            }
+        })(this._root);
+    },
+    traveseLRD: function (callback) {
+        (function recurse(current) {
+            if (current.children.length >= 1) {
+                recurse(current.children[0]);
+            }
+            if (current.children.length >= 2) {
+                recurse(current.children[1]);
+            }
+            callback.call(this, current);
+        })(this._root);
     }
 }
 
-function preOrder(node) {
-    if (node !== null) {
-        console.log(node.show());
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-}
+tree = new Tree($("a"));
 
 
-nodeA = new Node($("a"), $("a").children[0], $("a").children[1]);
 
-preOrder(nodeA);
+$("DLR").addEventListener("click", function () {
+    tree.traveseDLR(function (element) {
+        element.style.background = "#0cc";
+        setTimeout(function () {
+            element.style.background = "#fff";
+        }, 500)
+    });
+});
+
+$("LDR").addEventListener("click", function () {
+    tree.traveseLDR(function (element) {
+        console.log(element.id);
+        element.style.background = "#0cc";
+
+    });
+});
+
+$("LRD").addEventListener("click", function () {
+    tree.traveseLRD(function (element) {
+        console.log(element.id);
+        element.style.background = "#0cc";
+    });
+});
+
+console.log(tree);
