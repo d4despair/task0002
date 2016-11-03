62c8121346ceb33bcacc27201b1c034ac2511edf
diocai <caijiabin2008@gmail.com>
2016-11-03 17:12:07 +0800

    为了飞行

--- task26/js/main.js
+++ task26/js/main.js
@@ -2,6 +2,16 @@ function $(id) {
     return document.getElementById(id);
 }
 
+function addEvent(ele, event, handler) {
+    if (ele.addEventListener) {
+        ele.addEventListener(event, handler, false);
+    } else if (ele.attachEvent) {
+        ele.attachEvent("on" + event, handler);
+    } else {
+        ele["on" + event] = handler;
+    }
+}
+
 
 var stop = document.getElementById("stop");
 stop.addEventListener("click", function () {
@@ -47,32 +57,64 @@ $("destroy").addEventListener("click", function () {
     $("universe").remsoveChild($("ship1"));
 })
 
-
 function Spaceship(num) {
     this._id = num;
+    this.id = $("ship" + num);
     this.state = "STOP";
     this.fuel = 100;
     this.consumeRate = 5;
     this.chargeRate = 2;
+    this.speed = 5;
+    this.rate = 1000 / this.speed;
 }
 
 Spaceship.prototype = {
     launch: function () {
-
     },
     start: function () {
-
+        var that = this;
+        this.id.style.animationPlayState = "running";
+        this.state = "RUN";
+        var running = setInterval(function () {
+            console.log(that.fuel);
+            $("test").innerHTML = that.fuel + "%";
+            $("test").style.width = that.fuel + "%";
+            that.fuel -= that.consumeRate;
+            if (that.fuel <= 0) {
+                that.fuel = 0;
+                clearInterval(running);
+                console.log("run out");
+                $("test").innerHTML = that.fuel + "%";
+                $("test").style.width = that.fuel + "%";
+                that.stop();
+            }
+        }, this.rate)
     },
     stop: function () {
-
+        var that = this;
+        this.id.style.animationPlayState = "paused";
+        this.state = "STOP";
+        var recharge = setInterval(function () {
+            console.log(that.fuel);
+            $("test").innerHTML = that.fuel + "%";
+            $("test").style.width = that.fuel + "%";
+            that.fuel += that.chargeRate;
+            if (that.fuel >= 100) {
+                that.fuel = 100;
+                clearInterval(recharge);
+                console.log(that.fuel);
+                $("test").innerHTML = that.fuel + "%";
+                $("test").style.width = that.fuel + "%";
+            }
+        }, this.rate)
     },
     destroy: function () {
-
     }
 }
-
 var ship1 = new Spaceship(1);
-ship1.state = "RUNNING";
+
 var ship2 = new Spaceship(2);
+console.log(ship2.id);
+ship2.start();
 console.log(ship1.state);
 console.log(ship2.state);
