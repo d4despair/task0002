b4dd51c735bcda34e597b9c506c56c0bea2dd347
diocai <caijiabin2008@gmail.com>
2016-11-08 15:56:12 +0800

    新增拖动框雏形

--- task26/js/main.js
+++ task26/js/main.js
@@ -106,3 +106,56 @@ Spaceship.prototype = {
 var ship1 = new Spaceship(2);
 
 ship1.init();
+
+(function () {
+    var flag = 0;
+    var x1 = 0;
+    var y1 = 0;
+    var start = { x: new Number, y: new Number };
+    var moving = { x: new Number, y: new Number };
+
+    addEvent($("drag"), "mousedown", function () {
+        var target = event.target;
+        if (!flag) {
+            flag = 1;
+            x1 = event.clientX;
+            y1 = event.clientY;
+            left1 = target.style.left.replace("px", "");
+            top1 = target.style.top.replace("px", "");
+            console.log("drag!!!");
+        }
+        console.log($("drag").offsetHeight);
+        console.log($("drag").offsetWidth);
+    });
+
+    addEvent(document, "mousemove", function () {
+        var target = $("drag");
+        if (flag) {
+            var x = event.clientX;
+            var y = event.clientY;
+            var width = target.style.width;
+            var height = target.style.height;
+            var a = x1 - left1;
+            var b = y1 - top1;
+            target.style.left = x - a;
+            target.style.top = y - b;
+            if (x - a < 0) {
+                target.style.left = 0;
+            }
+            if (y - b < 0) {
+                target.style.top = 0;
+            }
+            if (x - a > (window.innerWidth - $("drag").offsetWidth)) {
+                target.style.left = window.innerWidth - $("drag").offsetWidth;
+            }
+            if (y - b > (window.innerHeight - $("drag").offsetHeight)) {
+                target.style.top = window.innerHeight - $("drag").offsetHeight;
+            }
+        }
+    })
+
+    addEvent(document, "mouseup", function () {
+        flag = 0;
+        console.log("drag done");
+    })
+})();
