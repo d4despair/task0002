774825f122b24bb55cc582f90a45740cbc4bf9db
diocai <caijiabin2008@gmail.com>
2016-11-11 16:56:12 +0800

    累觉不爱

--- task26/js/commander.js
+++ task26/js/commander.js
@@ -1,20 +1,41 @@
-addEvent($("createNewSpaceship"),"click",function () {
-    console.log("new ship launched!");
-    createNewSpaceship();
-    newSpaceshipLaunch();
-    consoleLog();
-});
+var ships = [];
+var msg = {};
 
-function createNewSpaceship(){
-    var controlList = $("control-list").innerHTML  ;
-    controlList+="<div><span>对1号飞船下达指令：</span><input id='start' type='button' value='开始飞行'><input id='stop' type='button' value='停止飞行'><input id='destroy' type='button' value='自爆'></div>";
+
+function createNewSpaceship(id) {
+    var len = ships.length;
+    ships[len] = new Spaceship(id);
+
+    var controlList = $("control-list").innerHTML;
+    controlList += "<div><span>对" + id + "号飞船下达指令：</span>"
+        + "<input data-command='start' data-id=" + id + " type='button' value='开始飞行'>"
+        + "<input data-command='stop' data-id=" + id + " type='button' value='停止飞行'>"
+        + "<input data-command='destruct' data-id=" + id + " type='button' value='自爆'></div>";
     $("control-list").innerHTML = controlList;
 }
 
-function newSpaceshipLaunch(){
-
+function consoleLog() {
+    $("log-content").innerHTML += getTime() + " " + "<span>新的飞船起飞了</span></br>";
 }
 
-function consoleLog(){
-    $("log-content").innerHTML += getTime()+" "+"<span>新的飞船起飞了</span></br>"; 
-}
+addEvent($("createNewSpaceship"), "click", function () {
+    console.log("new ship launched!");
+    var id = ships.length + 1;
+    createNewSpaceship(id);
+    consoleLog();
+});
+
+addEvent($("control-list"), "click", function (e) {
+    if (e.target.nodeName == 'INPUT') {
+        msg.id = e.target.dataset.id;
+        msg.command = e.target.dataset.command;
+        console.log(msg);
+    }
+    for (var i in ships) {
+        ships[i][msg.command].call(ships[i], msg.id);
+    }
+    if (msg.command == 'destruct') {
+        ships = ships.splice(0, msg.id - 1);
+        console.log(ships);
+    }
+})
--- task26/js/spaceship.js
+++ task26/js/spaceship.js
@@ -1,13 +1,83 @@
-var orbitRaiuses = {
-    orbit1:1
+/**
+ * 创建飞船原型
+ * @param id 飞船编号
+ */
+function Spaceship(id) {
+    this.id = id;
+    this.speed = 1;
+    this.energy = 100;
+    this.consumeRate = 5;
+    this.rechargeRate = 2;
+    this.status = 'STOP';
+    this.timer = null;
+
+    (function () {
+        var shipDiv = document.createElement('div');
+        shipDiv.id = 'ship' + id;
+        shipDiv.className = 'spaceship ship' + id;
+        document.body.appendChild(shipDiv);
+    })(id);
 }
 
-function Orbit(id){
-    this.radius = id;
-}
-
-
-function Spaceship(id){
-    this.id=id;
-    this.orbit=id;
+Spaceship.prototype = {
+    //公用属性
+    _energyMax: 100,
+    _energyMin: 0,
+    //动力系统
+    start: function (id) {
+        if (id == this.id) {
+            this.status = 'START';
+            $('ship'+id).style.animationPlayState = 'running';
+            this.consume();
+            console.log('ship' + this.id + ' start! speed: ' + this.speed + 'km/s, conumse rate: ' + this.consumeRate + '/s.');
+        }
+    },
+    stop: function (id) {
+        if (id == this.id) {
+            this.status = 'STOP';
+            $('ship'+id).style.animationPlayState = 'paused';
+            this.recharge();
+            console.log('ship' + this.id + ' stoped! recharge rate: ' + this.rechargeRate + '/s.');
+        }
+    },
+    //能源系统
+    consume: function () {
+        if (this.status == 'START') {
+            clearInterval(this.timer);
+            var that = this;
+            var consuming = setInterval(function () {
+                that.energy -= that.consumeRate;
+                if (that.energy < that._energyMin) {
+                    that.energy = that._energyMin;
+                    clearInterval(consuming);
+                    that.stop(that.id);
+                }
+                console.log('ship' + that.id + ' energy: ' + that.energy);
+            }, 1000);
+            this.timer = consuming;
+        }
+    },
+    recharge: function () {
+        if (this.status == 'STOP') {
+            clearInterval(this.timer);
+            var that = this;
+            var recharging = setInterval(function () {
+                that.energy += that.rechargeRate;
+                if (that.energy > that._energyMax) {
+                    that.energy = that._energyMax;
+                    clearInterval(recharging);
+                }
+                console.log('ship' + that.id + ' energy: ' + that.energy);
+            }, 1000)
+            this.timer = recharging;
+        }
+    },
+    //自爆系统
+    destruct: function (id) {
+        if (id == this.id) {
+            clearInterval(this.timer);
+            document.body.removeChild($('ship' + id));
+            console.log('boom! you gain the inner peace!');
+        }
+    }
 }
